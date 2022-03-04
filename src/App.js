c3210181448ca44a1307abc3bb0fe6c12ff65ec3
The Glass Corner <ksimons@aggies.ncat.edu>
2022-03-04 15:49:30 -0500

    added Transactions, Income expenses, updated Transaction List

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@
 import './App.css';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
+import { IncomeExp } from './components/IncomeExp';
+import { TransactionList } from './components/TransactionList';
+import { AddTransactions } from './components/AddTransactions';
 
 function App() {
   return (
     <div>
-    
+
      <Header />
      <div classname ="container">
       <Balance />
+      <IncomeExp />
+      <TransactionList />
+      <AddTransactions />
+      
     </div>
+    
 
     </div>
   );
