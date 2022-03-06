066df2ffc5e08a2ed1d3bbefccc3a9fb755039dd
The Glass Corner <ksimons@aggies.ncat.edu>
2022-03-06 13:31:52 -0500

    added ternary operators for negative and positive expenses, also if it is positive show green : red

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export const Transaction = ({transaction}) => { //transaction is a prop that is passed from TransactionList component
+  const sign = transaction.amount < 0 ? '-' : '+'; //if amount is negative, then sign is '-' else '+'
   return (
-    <li className="minus">
-    {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+    <li className={transaction.amount < 0 ? 'minus': 'plus'}>
+    {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
   </li>
   )
 }
